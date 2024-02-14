7089807abe652d22c74095df1cdf2a1f2396ee0a
kosfra <kosfrancuz@gmail.com>
2024-02-14 20:31:24 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a550178b-d74d-4b20-a054-ad8e9356e56f';
+export const MODAL_CLOSE_SEC = 2.5;
